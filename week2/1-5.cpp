90a26ff0dee059447a5d97bb58c9b7308b1f336b
ththfql <145094239+ththfql@users.noreply.github.com>
2026-03-11 17:30:13 +0900

    수업

---
+++ week2/1-5.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    {
+        std::string s = "a string";
+        {
+            std::string x = s + ", really";
+            std::cout << s << std::endl;
+            // 출력문을 x가 선언된 안으로 옮김
+            std::cout << x << std::endl; 
+        }
+    }
+    return 0;
+}
